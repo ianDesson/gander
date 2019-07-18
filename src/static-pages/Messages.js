98fc661991c2dbed55a963808686262ff1bd879a
ianDesson <ian.desson@outlook.com>
2019-07-17 22:53:38 -0400

    Adds colours to messages

--- src/static-pages/Messages.js
+++ src/static-pages/Messages.js
@@ -5,7 +5,16 @@ import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 
 import Add from "@material-ui/icons/Add";
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Divider } from "@material-ui/core";
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Divider
+} from "@material-ui/core";
+import { deepOrange } from '@material-ui/core/colors';
+
 
 export default class Messages extends React.Component {
   render() {
@@ -19,50 +28,46 @@ export default class Messages extends React.Component {
           style={{ margin: "0.5rem auto", width: "95%" }}
         >
           <IconButton>
-            <Avatar>
+            <Avatar style= {{ backgroundColor: '#80ff80' }}>
               <Add />
             </Avatar>
           </IconButton>
           <hr style={{ margin: "0 1rem 0 1rem" }} />
           <IconButton size="medium">
-            <Avatar>GA</Avatar>
+            <Avatar style={{ backgroundColor: "#ff1a1a" }}>GA</Avatar>
           </IconButton>
           <IconButton size="medium">
-            <Avatar>EN</Avatar>
+            <Avatar style={{ backgroundColor: "#4d4dff" }}>EN</Avatar>
           </IconButton>
           <IconButton size="medium">
-            <Avatar>EG</Avatar>
+            <Avatar style={{ backgroundColor: deepOrange[500] }}>EG</Avatar>
           </IconButton>
         </Grid>
         <hr />
         <List>
           <ListItem button>
             <ListItemAvatar>
-              <Avatar>
-                GA
-              </Avatar>
+              <Avatar style={{ backgroundColor: "#ff1a1a" }}>GA</Avatar>
             </ListItemAvatar>
             <ListItemText primary="Gaming Gang" secondary="76 members" />
           </ListItem>
           <Divider variant="inset" component="li" />
           <ListItem button>
             <ListItemAvatar>
-              <Avatar>
-                EN
-              </Avatar>
+              <Avatar style={{ backgroundColor: "#4d4dff" }}>EN</Avatar>
             </ListItemAvatar>
-            <ListItemText primary="Entrepeneurship Gang" secondary="23 members" />
+            <ListItemText
+              primary="Entrepeneurship Gang"
+              secondary="23 members"
+            />
           </ListItem>
           <Divider variant="inset" component="li" />
           <ListItem button>
             <ListItemAvatar>
-              <Avatar>
-                EG
-              </Avatar>
+              <Avatar style={{ backgroundColor: deepOrange[500] }}>EG</Avatar>
             </ListItemAvatar>
             <ListItemText primary="Engineering Gang" secondary="32 members" />
           </ListItem>
-
         </List>
       </>
     );
